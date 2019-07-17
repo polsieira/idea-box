baddef28cbf9e833c210918338ad36f0b92b0bca
posi7790 <polasp17@gmail.com>
2019-07-16 19:15:27 -0600

    Get star image to persist

--- main.js
+++ main.js
@@ -9,23 +9,52 @@ const cardHolder = document.querySelector('.section--display-ideas');
 
 // Event Listeners
 window.addEventListener('load', populateCards);
-main.addEventListener('click', ideaHandeler);
+main.addEventListener('click', ideaHandler);
+cardHolder.addEventListener('click', cardHandler);
 
 // Functions
 function populateCards(event) {
-  var ideas = JSON.parse(localStorage.getItem('ideaArray'));
-  console.log(ideas)
-  ideas.forEach(function(element) {
+  ideasTemp = JSON.parse(localStorage.getItem('ideaArray'));
+  ideasTemp.forEach(function(element) {
+    element = new Idea(element);
+    ideas.push(element);
+  })
+  if (ideas !== null) {
+    ideas.forEach(function(element) {
     buildCard(element)
-    console.log('here')
-  });
+    });
+  }
 }
 
-function ideaHandeler(event) {
+function ideaHandler(event) {
   event.preventDefault();
   if (event.target.classList.contains('button--save-idea')) {
     populateNewIdea();
   }
+}
+
+function cardHandler(event) {
+  event.preventDefault();
+  if (event.target.id === 'img img--delete-icon') {
+    deleteIdea(event);
+  }
+
+  if (event.target.id === 'img img--star-icon') {
+    toggleStar(event);
+  }
+}
+
+function toggleStar(event) {
+  var card = event.target.parentNode.parentNode;
+  var ideaIndex = locateIdea(card);
+  ideaIndex.updateStar();
+  ideaIndex.saveToStorage(ideas);
+  if(ideaIndex.star === true){
+    event.target.src = 'images/star-active.svg';
+  } else {
+    event.target.src = 'images/star.svg';
+  }
+  
 }
 
 function populateNewIdea() {
@@ -33,21 +62,48 @@ function populateNewIdea() {
   ideaText.id = Date.now();
   ideaText.title = titleInput.value;
   ideaText.body = bodyInput.value;
-  console.log(ideaText)
   var idea = new Idea(ideaText);
   idea.saveToStorage(ideas);
   buildCard(idea);
 }
 
 function buildCard(idea) {
+  var starImage = idea.star === true ? 'images/star-active.svg' : 'images/star.svg';
   cardHolder.insertAdjacentHTML('afterbegin', `
-    ${idea.title}`
+    <section class="section section--idea-card" data-id="${idea.id}">
+      <article class="article article--idea-header">
+        <img id="img img--star-icon" src=${starImage} alt="star icon">
+        <img id="img img--delete-icon" src="images/delete.svg" alt="delete icon">
+      </article>
+      <article class="article article--idea-content">
+        <h3 class="h3 h3--idea-header">${idea.title}</h3>
+        <p>${idea.body}</p>
+      </article>
+      <article class="article article--idea-footer">
+        <img src="images/upvote.svg" alt="upvote icon">
+        <p>Quality: Swill</p>
+        <img src="images/downvote.svg" alt="downvote icon">
+      </article>
+    </section>`
   )
 }
 
-function insertCard(idea) {
-
+function deleteIdea(event) {
+  var card = event.target.parentNode.parentNode;
+  cardHolder.removeChild(card);
+  var ideaIndex = locateIdea(card);
+  ideaIndex.deleteFromStorage(ideas);
 }
+
+function locateIdea(card) {
+  var index = ideas.findIndex(function(element) {
+    return element.id == card.dataset.id
+  });
+
+  return ideas[index];
+}
+
+
 
 
 function makeid(length) {
